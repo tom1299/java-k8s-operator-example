d9f455df1d68f68d2f3d08a1a22281e1102d4db4
tom1299 <tom1299@jeeatwork.com>
2021-01-28 09:54:33 +0100

    Initial commit

---
+++ operator-example/src/main/java/com/jeeatwork/k8s/ClientProvider.java
@@ -0,0 +1,27 @@
+package com.jeeatwork.k8s;
+
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ClientProvider {
+
+    @Produces
+    @Singleton
+    @Named("namespace")
+    private String findNamespace() throws IOException {
+        return new String(Files.readAllBytes(Paths.get("/var/run/secrets/kubernetes.io/serviceaccount/namespace")));
+    }
+
+    @Produces
+    @Singleton
+    KubernetesClient newClient(@Named("namespace") String namespace) {
+        return new DefaultKubernetesClient().inNamespace(namespace);
+    }
+}
---
+++ operator-example/src/main/java/com/jeeatwork/k8s/PodLister.java
@@ -0,0 +1,23 @@
+package com.jeeatwork.k8s;
+
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.runtime.StartupEvent;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.util.List;
+
+public class PodLister {
+
+    @Inject
+    private KubernetesClient client;
+
+    void onStartup(@Observes StartupEvent _ev) {
+        List<Pod> podList = client.pods().list().getItems();
+        System.out.println("Found " + podList.size() + " Pods:");
+        for (Pod pod : podList) {
+            System.out.println(" * " + pod.getMetadata().getName());
+        }
+    }
+}
