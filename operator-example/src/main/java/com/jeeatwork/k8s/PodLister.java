637cc852c3532c6a0278e0ae466f45e11b62f154
tom1299 <tom1299@jeeatwork.com>
2021-02-02 12:59:13 +0100

    Add periodic listing of pods

--- operator-example/src/main/java/com/jeeatwork/k8s/PodLister.java
+++ operator-example/src/main/java/com/jeeatwork/k8s/PodLister.java
@@ -3,12 +3,15 @@ package com.jeeatwork.k8s;
 import com.slack.api.bolt.App;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.quarkus.scheduler.Scheduled;
 import io.quarkus.runtime.StartupEvent;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import java.util.List;
 
+@ApplicationScoped
 public class PodLister {
 
     @Inject
@@ -17,11 +20,16 @@ public class PodLister {
     @Inject
     private App app;
 
-    void onStartup(@Observes StartupEvent _ev) {
+    @Scheduled(every="30s")
+    void listPods() {
         List<Pod> podList = client.pods().list().getItems();
         System.out.println("Found " + podList.size() + " Pods:");
         for (Pod pod : podList) {
             System.out.println(" * " + pod.getMetadata().getName());
         }
+    }
+
+    void onStartup(@Observes StartupEvent _ev) {
+        System.out.println("Starting...");
     }
 }
